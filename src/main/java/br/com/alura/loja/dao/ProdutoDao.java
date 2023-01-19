8d1760ce7bae0834cd5c3e6462a017ecf2b338a9
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2023-01-19 17:59:11 -0300

    Mapeando entidades - Organizando o código

---
+++ src/main/java/br/com/alura/loja/dao/ProdutoDao.java
@@ -0,0 +1,16 @@
+package br.com.alura.loja.dao;
+
+import br.com.alura.loja.modelo.Produto;
+
+import javax.persistence.EntityManager;
+
+public class ProdutoDao {
+    private EntityManager em;
+
+    public ProdutoDao(EntityManager em) {
+        this.em = em;
+    }
+    public void cadastrar(Produto produto){
+        this.em.persist(produto);
+    }
+}
--- src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -1,6 +1,8 @@
 package br.com.alura.loja.testes;
 
+import br.com.alura.loja.dao.ProdutoDao;
 import br.com.alura.loja.modelo.Produto;
+import br.com.alura.loja.util.JPAUtil;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -14,12 +16,11 @@ public class CadastroDeProduto {
         celular.setDescricao("Muito legal");
         celular.setPreco(new BigDecimal("800"));
 
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("loja");
-
-        EntityManager em = factory.createEntityManager();
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDao dao = new ProdutoDao(em);
 
         em.getTransaction().begin();
-        em.persist(celular);
+        dao.cadastrar(celular);
         em.getTransaction().commit();
         em.close();
     }
