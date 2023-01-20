9aa00d0ffd5efd20831c1330c1ef809257151da7
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2023-01-20 16:02:53 -0300

    Mais relacionamentos - Mapeando novas entidades

---
+++ src/main/java/br/com/alura/loja/modelo/Pedido.java
@@ -0,0 +1,50 @@
+package br.com.alura.loja.modelo;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "pedidos")
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal valorTotal;
+    private LocalDate data = LocalDate.now();
+
+    @ManyToOne
+    private Cliente cliente;
+
+    public Pedido(){
+    }
+
+    public Pedido(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+}
