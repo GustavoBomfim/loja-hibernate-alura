b8abc210e83644b881fcd6a8c4b8635536f15758
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2023-01-31 16:35:12 -0300

    Criteria API - Consultas com parametro dinamico

--- src/main/java/br/com/alura/loja/dao/ProdutoDao.java
+++ src/main/java/br/com/alura/loja/dao/ProdutoDao.java
@@ -3,7 +3,9 @@ package br.com.alura.loja.dao;
 import br.com.alura.loja.modelo.Produto;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 public class ProdutoDao {
@@ -42,5 +44,33 @@ public class ProdutoDao {
         return em.createQuery(jpql, BigDecimal.class)
                 .setParameter("nome", nome)
                 .getSingleResult();
+    }
+
+    public List<Produto> buscarPorParametros(String nome, BigDecimal preco, LocalDate dataCadastro){
+        String jpql = "SELECT p FROM Produto p WHERE 1=1 ";
+
+        if(nome != null && !nome.trim().isEmpty()){
+            jpql = jpql.concat(" AND p.nome = :nome ");
+        }
+        if(preco != null){
+            jpql = jpql.concat(" AND p.preco = :preco ");
+        }
+        if(dataCadastro != null){
+            jpql = jpql.concat(" AND p.dataCadastro = :dataCadastro ");
+        }
+
+
+        TypedQuery<Produto> query = em.createQuery(jpql, Produto.class);
+
+        if(nome != null && !nome.trim().isEmpty()){
+            query.setParameter("nome", nome);
+        }
+        if(preco != null){
+            query.setParameter("preco", preco);
+        }
+        if(dataCadastro != null){
+            query.setParameter("dataCadastro", dataCadastro);
+        }
+        return query.getResultList();
     }
 }
